ecd40fbc374a5a22f54ae1903be8ac3d496973e5
Jakub <jakub.holasek@live.com>
2026-09-19 10:54:05 +0200

    Fix BSEED mains-client OTA abort recovery (#31)
    * Fix mains-client OTA abort recovery
    
    * Wire mains-client OTA retry task
    
    * Apply OTA recovery formatting
    
    * Fix OTA recovery test formatting

--- src/telink/hal/zigbee_ota.c
+++ src/telink/hal/zigbee_ota.c
@@ -6,6 +6,7 @@
 
 #include "telink_size_t_hack.h"
 
+#include "hal/tasks.h"
 #include "hal/zigbee_ota.h"
 #include "telink_zigbee_hal.h"
 #include "version_cfg.h"
@@ -13,6 +14,17 @@
 // Forward declarations
 
 void ota_process_msg_callback(u8 evt, u8 status);
+
+#ifdef BSEED_MAINS_CLIENT
+#define OTA_ABORT_QUERY_RETRY_DELAY_MS    1000
+static hal_task_t ota_abort_query_retry_task;
+
+static void ota_abort_query_retry(void *arg) {
+    (void)arg;
+    ota_queryStart(OTA_PERIODIC_QUERY_INTERVAL);
+}
+
+#endif
 
 // ota data structs
 
@@ -36,16 +48,36 @@ void hal_ota_cluster_setup(hal_zigbee_cluster *cluster) {
 }
 
 void ota_process_msg_callback(u8 evt, u8 status) {
+#ifdef BSEED_MAINS_CLIENT
+    if (evt == OTA_EVT_START) {
+        hal_tasks_unschedule(&ota_abort_query_retry_task);
+        return;
+    }
+#endif
+
     if (evt == OTA_EVT_COMPLETE) {
         if (status == ZCL_STA_SUCCESS) {
             ota_mcuReboot();
         } else {
+#ifdef BSEED_MAINS_CLIENT
+            // Telink uses one shared OTA timer for block-response waits and
+            // periodic queries. Restart querying from a separate application
+            // timer so an abort callback cannot race the shared timer cleanup.
+            hal_tasks_schedule(&ota_abort_query_retry_task,
+                               OTA_ABORT_QUERY_RETRY_DELAY_MS);
+#else
             ota_queryStart(OTA_PERIODIC_QUERY_INTERVAL);
+#endif
         }
     }
 }
 
 void hal_zigbee_init_ota() {
+#ifdef BSEED_MAINS_CLIENT
+    hal_tasks_init(&ota_abort_query_retry_task);
+    ota_abort_query_retry_task.handler = ota_abort_query_retry;
+    ota_abort_query_retry_task.arg     = NULL;
+#endif
     // This registers OTA cluster in ZCL and does all SDK-internal setup
     ota_init(OTA_TYPE_CLIENT, telink_zigbee_hal_zcl_get_descriptors(),
              &ota_preamble, &ota_callback);
